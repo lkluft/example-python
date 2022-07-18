5ad30bd943edb3d3c2fc860406f0f7f8914b1887
Lukas Kluft <lukas.kluft@gmail.com>
2022-07-18 12:02:37 +0200

    Add sphinx infrastructure

--- mypackage/functions.py
+++ mypackage/functions.py
@@ -1,2 +1,11 @@
 def add(a, b):
+    """Add two numbers `a` and `b`.
+
+    Parameters:
+        a (float): First number.
+        b (float): Second number.
+
+    Returns:
+        float
+    """
     return a + b
